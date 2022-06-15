f0bea2cef3494a130cc5b74234933e12b9d2067f
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-06-15 13:59:31 +0530

    Two Sum Sol

---
+++ Two_Sum.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        map<int, int> mp;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int tar = target - nums[i];
+            if (mp.find(tar) != mp.end())
+            {
+                return {i, mp[tar]};
+            }
+            mp[nums[i]] = i;
+        }
+        return {};
+    }
+};
